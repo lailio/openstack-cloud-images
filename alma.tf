7bd8858410b3a2fb43b327e6c72294fae30159ca
Pēteris Elksnis <peteris@lailio.com>
2025-01-16 17:55:11 +0200

    Alma and Ubuntu fixes

--- alma.tf
+++ alma.tf
@@ -1,3 +1,45 @@
 #
 # https://repo.almalinux.org/almalinux/
 #
+
+resource "openstack_images_image_v2" "AlmaLinux-8-GenericCloud-latest_x86_64" {
+  name             = "AlmaLinux-8-GenericCloud-latest.x86_64"
+  image_source_url = "https://repo.almalinux.org/almalinux/8/cloud/x86_64/images/AlmaLinux-8-GenericCloud-latest.x86_64.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  web_download     = true
+  protected        = true
+  visibility       = "public"
+}
+
+resource "openstack_images_image_v2" "AlmaLinux-8-GenericCloud-latest_aarch64" {
+  name             = "AlmaLinux-8-GenericCloud-latest.aarch64"
+  image_source_url = "https://repo.almalinux.org/almalinux/8/cloud/aarch64/images/AlmaLinux-8-GenericCloud-latest.aarch64.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  web_download     = true
+  protected        = true
+  visibility       = "public"
+}
+
+# ---
+
+resource "openstack_images_image_v2" "AlmaLinux-9-GenericCloud-latest_x86_64" {
+  name             = "AlmaLinux-9-GenericCloud-latest.x86_64"
+  image_source_url = "https://repo.almalinux.org/almalinux/9/cloud/x86_64/images/AlmaLinux-9-GenericCloud-latest.x86_64.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  web_download     = true
+  protected        = true
+  visibility       = "public"
+}
+
+resource "openstack_images_image_v2" "AlmaLinux-9-GenericCloud-latest_aarch64" {
+  name             = "AlmaLinux-9-GenericCloud-latest.aarch64"
+  image_source_url = "https://repo.almalinux.org/almalinux/9/cloud/aarch64/images/AlmaLinux-9-GenericCloud-latest.aarch64.qcow2"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  web_download     = true
+  protected        = true
+  visibility       = "public"
+}
--- ubuntu.minimal.tf
+++ ubuntu.minimal.tf
@@ -1,7 +1,7 @@
 #
 # https://cloud-images.ubuntu.com/daily/server/minimal/releases/
 #
-resource "openstack_images_image_v2" "ubuntu-16.04-minimal-cloudimg-amd64-disk1" {
+resource "openstack_images_image_v2" "ubuntu-16_04-minimal-cloudimg-amd64-disk1" {
   name             = "ubuntu-16.04-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/xenial/release/ubuntu-16.04-minimal-cloudimg-amd64-disk1.img"
   container_format = "bare"
@@ -10,16 +10,8 @@ resource "openstack_images_image_v2" "ubuntu-16.04-minimal-cloudimg-amd64-disk1"
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-16.04-minimal-cloudimg-amd64-uefi1" {
-  name             = "ubuntu-16.04-minimal-amd64"
-  image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/xenial/release/ubuntu-16.04-minimal-cloudimg-amd64-uefi1.img"
-  container_format = "bare"
-  disk_format      = "qcow2"
-  web_download     = true
-  protected        = true
-  visibility       = "public"
-}
-resource "openstack_images_image_v2" "ubuntu-18.04-minimal-cloudimg-amd64" {
+
+resource "openstack_images_image_v2" "ubuntu-18_04-minimal-cloudimg-amd64" {
   name             = "ubuntu-18.04-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/bionic/release/ubuntu-18.04-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -28,7 +20,7 @@ resource "openstack_images_image_v2" "ubuntu-18.04-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-18.10-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-18_10-minimal-cloudimg-amd64" {
   name             = "ubuntu-18.10-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/cosmic/release/ubuntu-18.10-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -37,7 +29,7 @@ resource "openstack_images_image_v2" "ubuntu-18.10-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-19.04-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-19_04-minimal-cloudimg-amd64" {
   name             = "ubuntu-19.04-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/disco/release/ubuntu-19.04-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -46,7 +38,7 @@ resource "openstack_images_image_v2" "ubuntu-19.04-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-19.10-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-19_10-minimal-cloudimg-amd64" {
   name             = "ubuntu-19.10-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/eoan/release/ubuntu-19.10-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -55,7 +47,7 @@ resource "openstack_images_image_v2" "ubuntu-19.10-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-20.04-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-20_04-minimal-cloudimg-amd64" {
   name             = "ubuntu-20.04-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/focal/release/ubuntu-20.04-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -64,16 +56,8 @@ resource "openstack_images_image_v2" "ubuntu-20.04-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-20.04-minimal-cloudimg-amd64" {
-  name             = "ubuntu-20.04-minimal-amd64"
-  image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/groovy/release/ubuntu-20.10-minimal-cloudimg-amd64.img"
-  container_format = "bare"
-  disk_format      = "qcow2"
-  web_download     = true
-  protected        = true
-  visibility       = "public"
-}
-resource "openstack_images_image_v2" "ubuntu-21.04-minimal-cloudimg-amd64" {
+
+resource "openstack_images_image_v2" "ubuntu-21_04-minimal-cloudimg-amd64" {
   name             = "ubuntu-21.04-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/hirsute/release/ubuntu-21.04-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -82,7 +66,7 @@ resource "openstack_images_image_v2" "ubuntu-21.04-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-21.10-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-21_10-minimal-cloudimg-amd64" {
   name             = "ubuntu-21.10-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/impish/release/ubuntu-21.10-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -91,7 +75,7 @@ resource "openstack_images_image_v2" "ubuntu-21.10-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-22.04-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-22_04-minimal-cloudimg-amd64" {
   name             = "ubuntu-22.04-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/jammy/release/ubuntu-22.04-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -100,7 +84,7 @@ resource "openstack_images_image_v2" "ubuntu-22.04-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-22.10-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-22_10-minimal-cloudimg-amd64" {
   name             = "ubuntu-22.10-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/kinetic/release/ubuntu-22.10-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -109,7 +93,7 @@ resource "openstack_images_image_v2" "ubuntu-22.10-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-23.04-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-23_04-minimal-cloudimg-amd64" {
   name             = "ubuntu-23.04-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/lunar/release/ubuntu-23.04-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -118,7 +102,7 @@ resource "openstack_images_image_v2" "ubuntu-23.04-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-23.10-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-23_10-minimal-cloudimg-amd64" {
   name             = "ubuntu-23.10-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/mantic/release/ubuntu-23.10-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -127,7 +111,7 @@ resource "openstack_images_image_v2" "ubuntu-23.10-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-23.10-minimal-cloudimg-arm64" {
+resource "openstack_images_image_v2" "ubuntu-23_10-minimal-cloudimg-arm64" {
   name             = "ubuntu-23.10-minimal-arm64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/mantic/release/ubuntu-23.10-minimal-cloudimg-arm64.img"
   container_format = "bare"
@@ -136,7 +120,7 @@ resource "openstack_images_image_v2" "ubuntu-23.10-minimal-cloudimg-arm64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-24.04-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-24_04-minimal-cloudimg-amd64" {
   name             = "ubuntu-24.04-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/noble/release/ubuntu-24.04-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -145,7 +129,7 @@ resource "openstack_images_image_v2" "ubuntu-24.04-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-24.04-minimal-cloudimg-arm64" {
+resource "openstack_images_image_v2" "ubuntu-24_04-minimal-cloudimg-arm64" {
   name             = "ubuntu-24.04-minimal-arm64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/noble/release/ubuntu-24.04-minimal-cloudimg-arm64.img"
   container_format = "bare"
@@ -154,7 +138,7 @@ resource "openstack_images_image_v2" "ubuntu-24.04-minimal-cloudimg-arm64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-24.10-minimal-cloudimg-amd64" {
+resource "openstack_images_image_v2" "ubuntu-24_10-minimal-cloudimg-amd64" {
   name             = "ubuntu-24.10-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/oracular/release/ubuntu-24.10-minimal-cloudimg-amd64.img"
   container_format = "bare"
@@ -163,7 +147,7 @@ resource "openstack_images_image_v2" "ubuntu-24.10-minimal-cloudimg-amd64" {
   protected        = true
   visibility       = "public"
 }
-resource "openstack_images_image_v2" "ubuntu-24.10-minimal-cloudimg-arm64" {
+resource "openstack_images_image_v2" "ubuntu-24_10-minimal-cloudimg-arm64" {
   name             = "ubuntu-24.10-minimal-arm64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/oracular/release/ubuntu-24.10-minimal-cloudimg-arm64.img"
   container_format = "bare"
