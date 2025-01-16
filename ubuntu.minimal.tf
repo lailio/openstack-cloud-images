ecb16b59d875160711496a47f6571344d43b9b3e
jekabskrastins <153902556+jekabskrastins@users.noreply.github.com>
2025-01-16 16:11:05 +0200

    Update ubuntu.minimal.tf

--- ubuntu.minimal.tf
+++ ubuntu.minimal.tf
@@ -10,3 +10,12 @@ resource "openstack_images_image_v2" "ubuntu-16.04-minimal-cloudimg-amd64-disk1"
   protected        = true
   visibility       = "public"
 }
+resource "openstack_images_image_v2" "ubuntu-18.04-minimal-cloudimg-amd64" {
+  name             = "ubuntu-18.04-minimal-cloudimg-amd64"
+  image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/bionic/release/ubuntu-18.04-minimal-cloudimg-amd64.img"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  web_download     = true
+  protected        = true
+  visibility       = "public"
+}
