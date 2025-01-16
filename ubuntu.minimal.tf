0fe4b1013539c74f4a015d9d2da83483cbbbb456
Pēteris Elksnis <peteris@lailio.com>
2025-01-16 10:03:27 +0200

    Add ubuntu minimal repo

---
+++ ubuntu.minimal.tf
@@ -0,0 +1,12 @@
+#
+# https://cloud-images.ubuntu.com/daily/server/minimal/releases/
+#
+resource "openstack_images_image_v2" "ubuntu-16.04-minimal-cloudimg-amd64-disk1" {
+  name             = "ubuntu-16.04-minimal-amd64"
+  image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/xenial/release/ubuntu-16.04-minimal-cloudimg-amd64-disk1.img"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  web_download     = true
+  protected        = true
+  visibility       = "public"
+}
