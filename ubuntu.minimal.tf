3f1316c84cc5835552df4a004c5fd2e87fcaf0e8
jekabs krastins <jekabskrastins@jekabss-MacBook-Pro.local>
2025-01-16 17:09:49 +0200

    ubuntu.minimal one more

--- ubuntu.minimal.tf
+++ ubuntu.minimal.tf
@@ -10,6 +10,15 @@ resource "openstack_images_image_v2" "ubuntu-16.04-minimal-cloudimg-amd64-disk1"
   protected        = true
   visibility       = "public"
 }
+resource "openstack_images_image_v2" "ubuntu-16.04-minimal-cloudimg-amd64-uefi1" {
+  name             = "ubuntu-16.04-minimal-amd64"
+  image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/xenial/release/ubuntu-16.04-minimal-cloudimg-amd64-uefi1.img"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  web_download     = true
+  protected        = true
+  visibility       = "public"
+}
 resource "openstack_images_image_v2" "ubuntu-18.04-minimal-cloudimg-amd64" {
   name             = "ubuntu-18.04-minimal-amd64"
   image_source_url = "https://cloud-images.ubuntu.com/daily/server/minimal/releases/bionic/release/ubuntu-18.04-minimal-cloudimg-amd64.img"
